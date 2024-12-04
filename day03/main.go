c992a95a4ef59d394428999f36d18583001dd04a
atchman <atchman@users.noreply.github.com>
2024-12-04 01:29:34 +0100

    feat(day02): WIP part 2 sample test is ok

--- day03/main.go
+++ day03/main.go
@@ -11,6 +11,7 @@ import (
 func main() {
     // result
     fmt.Println("Part 1:", One(parseFile("input")))
+    fmt.Println("Part 2:", Two(parseFile("input")))
 }
 
 func parseFile(file string) (mem string) {
@@ -45,6 +46,50 @@ func One(mem string) (sum int) {
         if err != nil {
             log.Fatalf("convert str to int, a: %v", err)
         }
+        // b
+        b, err := strconv.Atoi(numbers[1])
+        if err != nil {
+            log.Fatalf("convert str to int, b: %v", err)
+        }
+        sum = sum + (a * b)
+    }
+    return sum
+}
+
+// Part Two
+func Two(mem string) (sum int) {
+    sum = 0
+
+    // disable regexp
+    dis, err := regexp.Compile(`don\'t\(\).*?(?:do\(\)|$)`)
+    if err != nil {
+        log.Fatalf("regexp dis: %v", err)
+    }
+    get_clean := dis.ReplaceAllString(mem, " ")
+
+    // get mul
+    mul, err := regexp.Compile(`mul\([0-9]{1,3},[0-9]{1,3}\)`)
+    if err != nil {
+        log.Fatalf("regexp mul: %v", err)
+    }
+    // get list of all finds
+    get_mul := mul.FindAllString(get_clean, -1)
+
+    for _, mul := range get_mul {
+        re, err := regexp.Compile(`[0-9]{1,3}`)
+        if err != nil {
+            log.Fatalf("regexp for number: %v", err)
+        }
+
+        // regexp to get numbers only
+        numbers := (re.FindAllString(mul, -1))
+
+        // a
+        a, err := strconv.Atoi(numbers[0])
+        if err != nil {
+            log.Fatalf("convert str to int, a: %v", err)
+        }
+
         // b
         b, err := strconv.Atoi(numbers[1])
         if err != nil {
--- day03/main_test.go
+++ day03/main_test.go
@@ -11,3 +11,11 @@ func TestOne(t *testing.T) {
 		t.Errorf("Sum = %d; want 161", sum)
 	}
 }
+
+// test part 2
+func TestTwo(t *testing.T) {
+	sum := Two(parseFile("input.sample"))
+	if 48 != sum {
+		t.Errorf("Sum = %d; want 48", sum)
+	}
+}
